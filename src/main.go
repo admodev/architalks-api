e70a88d2ef822125428f2c7ab327a6eb17565a9c
admodev <admodevcodes@outlook.com>
2022-03-14 00:12:28 -0300

    add envs

--- src/main.go
+++ src/main.go
@@ -3,15 +3,26 @@ package main
 import (
     "time"
     "log"
+    "os"
     "net/http"
     "architalks/routes"
     "database/sql"
+    "github.com/joho/godotenv"
     "github.com/gorilla/mux"
     _ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-    db, err := sql.Open("mysql", "user:password@/database")
+    godotenv.Load(".env")
+
+    database_driver := os.Getenv("DB_DRIVER")
+    database_user := os.Getenv("DB_USER")
+    database_password := os.Getenv("DB_PASSWORD")
+    database := os.Getenv("DB_NAME")
+
+    var connectionStr string = database_user + ":" + database_password + "@/" + database
+
+    db, err := sql.Open(database_driver, connectionStr)
 
     if err != nil {
         panic(err)
