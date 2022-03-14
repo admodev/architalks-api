d4159ecbe9420c2f660236e6aeb5659ec0974715
admodev <admodevcodes@outlook.com>
2022-03-13 23:54:11 -0300

    add user model, add user route

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -3,8 +3,21 @@ package routes
 import (
     "fmt"
     "net/http"
+    "architalks/models"
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println("Ping!~")
+}
+
+func getUser(r *http.Request) models.User {
+    name := r.FormValue("name")
+    email := r.FormValue("email")
+    password := r.FormValue("password")
+
+    return models.User{
+        Name:    name,
+        Email:   email,
+        Password:    password,
+    }
 }
